641bb84a67f932eb2c1b2b1c849969412103e137
wangjiawen2013 <wangjiawen2013@163.com>
2026-01-31 20:09:01 +0800

    rule chart

--- src/render.rs
+++ src/render.rs
@@ -3,5 +3,6 @@ pub(crate) mod point_renderer;
 pub(crate) mod line_renderer;
 pub(crate) mod errorbar_renderer;
 pub(crate) mod text_renderer;
+pub(crate) mod rule_renderer;
 pub(crate) mod axis_renderer;
 pub(crate) mod legend_renderer;
