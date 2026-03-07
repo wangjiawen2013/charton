84ca5377a42011d3d62d78abeac3e2b4d40a4553
wangjiawen2013 <wangjiawen2013@163.com>
2026-03-07 14:33:06 +0800

    grouped_bar_with_errorbar

---
+++ examples/grouped_bar_with_errorbar.rs
@@ -0,0 +1,30 @@
+use charton::prelude::*;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let df = load_dataset("penguins")?;
+    println!("{:?}", df);
+
+    // 2. Build the Error Bar layer
+    let errorbar_layer = Chart::build(&df)?
+        .mark_errorbar()?
+        // Mapping 'Sex' to color triggers the dodge logic
+        .encode((x("Species"), y("Body Mass (g)"), color("Sex")))?;
+
+    // 3. Add a Bar layer to see the alignment
+    let bar_layer = Chart::build(&df)?.mark_bar()?.encode((
+        x("Species"),
+        y("Body Mass (g)").with_aggregate("mean"),
+        color("Sex"),
+    ))?;
+
+    // 4. Create the Layered Chart
+    LayeredChart::new()
+        .with_size(600, 400)
+        .with_title("Grouped Error Bars with Mean & Std Dev")
+        .add_layer(errorbar_layer) // Error bars on top
+        .add_layer(bar_layer) // Layer bars first
+        .save("./examples/grouped_bar_with_errorbar.svg")?;
+
+    Ok(())
+}
