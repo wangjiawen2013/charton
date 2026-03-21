ddaa443bdc747d49b778458e9ecc2107d7d5e7cf
wangjiawen2013 <wangjiawen2013@163.com>
2026-03-21 10:06:45 +0800

    tick chart book

--- examples/strip.rs
+++ examples/strip.rs
@@ -4,14 +4,12 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let df = load_dataset("iris")?;
 
-    let chart = Chart::build(&df)?
-        .mark_tick()?
-        .encode((x("sepal_width"), y("species"), color("species")))?
-        .configure_tick(|m| {
-            m.with_thickness(2.0)
-                .with_band_size(10.0)
-                .with_color("blue")
-        });
+    let chart = Chart::build(&df)?.mark_tick()?.encode((
+        x("sepal_width"),
+        y("species"),
+        color("species"),
+    ))?;
+
     chart.save("docs/src/images/strip.svg")?;
 
     Ok(())
