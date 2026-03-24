8925aa799b7985b272a922dc3d3044e860a616e6
wangjiawen2013 <wangjiawen2013@163.com>
2026-03-24 22:38:49 +0800

    area plot

---
+++ examples/normalized_stacked_area_chart.rs
@@ -0,0 +1,16 @@
+use charton::prelude::*;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let df = load_dataset("unemployment")?;
+    println!("{}", df);
+    let area_chart = Chart::build(&df)?.mark_area()?.encode((
+        x("Year"),
+        y("Unemployment rate (%)").with_stack("normalize"),
+        color("Country"),
+    ))?;
+
+    area_chart.save("docs/src/images/normalized_stacked_area.svg")?;
+
+    Ok(())
+}
