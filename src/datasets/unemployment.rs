d7ab4b8be2c57d8fa5c7a208428ddf6fffaa7cdc
wangjiawen2013 <wangjiawen2013@163.com>
2026-03-24 22:38:49 +0800

    area plot

--- src/datasets/unemployment.rs
+++ src/datasets/unemployment.rs
@@ -1,6 +1,6 @@
 use crate::error::ChartonError;
 use polars::prelude::*;
-use time::OffsetDateTime;
+//use time::OffsetDateTime;
 
 // unemployment dataset: A sub set of country's unemployment rate from past 31 years from https://www.kaggle.com/datasets/pantanjali/unemployment-dataset
 pub fn get_data() -> Result<DataFrame, ChartonError> {
@@ -62,7 +62,7 @@ pub fn get_data() -> Result<DataFrame, ChartonError> {
         "China",
         "Cameroon",
     ];
-    let year_raw = [
+    let year = [
         2014, 2014, 2014, 2014, 2014, 2014, 2014, 2015, 2015, 2015, 2015, 2015, 2015, 2015, 2016,
         2016, 2016, 2016, 2016, 2016, 2016, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2018, 2018,
         2018, 2018, 2018, 2018, 2018, 2019, 2019, 2019, 2019, 2019, 2019, 2019, 2020, 2020, 2020,
@@ -75,23 +75,24 @@ pub fn get_data() -> Result<DataFrame, ChartonError> {
         4.82, 11.18, 5.00, 3.84, 13.28, 1.79, 7.51, 5.32, 9.13, 4.82, 3.87,
     ];
 
-    let date_values: Vec<i64> = year_raw
-        .iter()
-        .map(|&year| {
-            let dt = OffsetDateTime::new_utc(
-                time::Date::from_calendar_date(year, time::Month::January, 1).unwrap(),
-                time::Time::MIDNIGHT,
-            );
-            dt.unix_timestamp_nanos() as i64
-        })
-        .collect();
+    // Convert year to datetime in the future
+    // let date_values: Vec<i64> = year_raw
+    //     .iter()
+    //     .map(|&year| {
+    //         let dt = OffsetDateTime::new_utc(
+    //             time::Date::from_calendar_date(year, time::Month::January, 1).unwrap(),
+    //             time::Time::MIDNIGHT,
+    //         );
+    //         dt.unix_timestamp_nanos() as i64
+    //     })
+    //     .collect();
 
-    let year_series = Series::new("Year".into(), date_values)
-        .cast(&DataType::Datetime(TimeUnit::Nanoseconds, None))?;
+    // let year_series = Series::new("Year".into(), date_values)
+    //     .cast(&DataType::Datetime(TimeUnit::Nanoseconds, None))?;
 
     let df = df![
      "Country" => country,
-     "Year" => year_series,
+     "Year" => year,
      "Unemployment rate (%)" => unemployment
     ]?;
 
