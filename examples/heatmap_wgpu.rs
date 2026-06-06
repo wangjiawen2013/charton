1cf22b312995cacdb9235f9f04d051cae348a4e2
wangjiawen2013 <wangjiawen2013@163.com>
2026-06-06 12:57:45 +0800

    colorbar

---
+++ examples/heatmap_wgpu.rs
@@ -0,0 +1,17 @@
+use charton::prelude::*;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Sample data for heatmap
+    let x = ["A", "B", "C", "A", "B", "C", "A", "B", "C"];
+    let y = ["X", "X", "X", "Y", "Y", "Y", "Z", "Z", "Z"];
+    let value = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+
+    // Create heatmap chart
+    chart!(x, y, value)?
+        .mark_rect()?
+        .encode((alt::x("x"), alt::y("y"), alt::color("value")))?
+        .save("heatmap_gpu.png")?;
+
+    Ok(())
+}
